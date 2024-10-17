2c7d1b7b45a0182b1f14a75093cdbd50a45930d2
ABBEY AINA <olamilekansunday445@gmail.com>
2024-10-17 09:38:17 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
                 className={`p-2 mb-4 ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'}`}
             >
                 <img 
-                    src="src/components/9071075_dark_mode_icon.svg" 
+                    src="./components/9071075_dark_mode_icon.svg" 
                     alt="Darkmode" 
                     className="darkmodeicon inline-block h-9 w-9"
                 />
